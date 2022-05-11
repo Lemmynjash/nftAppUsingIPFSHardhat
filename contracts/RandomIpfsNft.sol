e551addae9d5cea5e5cf81e4a401eee9022a3a9d
Lemmynjash <lemmynjariakiarie@gmail.com>
2022-05-11 23:34:06 +0300

    //Mint a random nft(puppy)

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -9,12 +9,33 @@ contract RandomIpfsNft is ERC721URIStorage, VRFConsumerBaseV2 {
     //why use immutable because its gas efficient
     //plus we are going to use the variable once
     VRFCoordinatorV2Interface immutable vrfCordinator2;
+    bytes32 immutable gasLane;
+    uint64 immutable subscriptionID;
+    uint16 constant REQUESTCONFIRMATION = 7;
+    uint32 immutable callbackGasLimit;
+    uint16 constant NUM_WORDS = 3;
 
     //nfts will be called Random IPFS NFT and NFT token is called RIN
-    constructor(address vrfCordinator2) ERC721("Random IPFS NFT", "RIN") {
+    constructor(
+        address vrfCordinator2,
+        bytes32 gasLane,
+        uint64 subscriptionID,
+        uint32 callbackGasLimit
+    ) ERC721("Random IPFS NFT", "RIN") VRFConsumerBaseV2(vrfCordinator2) {
         vrfCordinator2 = VRFCoordinatorV2Interface(vrfCordinator2);
+        gasLane = gasLane;
+        subscriptionID = subscriptionID;
+        callbackGasLimit = callbackGasLimit;
     }
 
     //Mint a random nft(puppy)
-    function requestADog() public returns (uint256 requestId) {}
+    function requestADog() public returns (uint256 requestId) {
+        requestId=vrfCordinator2.requestRandomWords(
+            gasLane,
+            subscriptionID,
+            REQUESTCONFIRMATION,
+            callbackGasLimit,
+            NUM_WORDS,
+        );
+    }
 }
