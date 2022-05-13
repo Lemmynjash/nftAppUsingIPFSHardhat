217c563880e83ff8046b96931b7d444faa492b1b
Lemmynjash <lemmynjariakiarie@gmail.com>
2022-05-13 09:24:14 +0300

    fullfillRandomWords and adding token URI for actual minting

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -15,6 +15,10 @@ contract RandomIpfsNft is ERC721URIStorage, VRFConsumerBaseV2 {
     uint32 immutable callbackGasLimit;
     uint16 constant NUM_WORDS = 3;
 
+    mapping(uint256 => address) s_requestIdToSender;
+
+    uint256 s_tokenCounter;
+
     //nfts will be called Random IPFS NFT and NFT token is called RIN
     constructor(
         address vrfCordinator2,
@@ -26,16 +30,36 @@ contract RandomIpfsNft is ERC721URIStorage, VRFConsumerBaseV2 {
         gasLane = gasLane;
         subscriptionID = subscriptionID;
         callbackGasLimit = callbackGasLimit;
+        s_tokenCounter = 0;
     }
 
     //Mint a random nft(puppy)
     function requestADog() public returns (uint256 requestId) {
-        requestId=vrfCordinator2.requestRandomWords(
+        requestId = vrfCordinator2.requestRandomWords(
+            //price for gas
             gasLane,
             subscriptionID,
             REQUESTCONFIRMATION,
+            //maximum gas amount
             callbackGasLimit,
-            NUM_WORDS,
+            NUM_WORDS
         );
+
+        s_requestIdToSender[requestId] = msg.sender;
+    }
+
+    function fullfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randonWords
+    ) internal override {
+        //here we need to mint
+        //this line defines the owner of the dog
+        address dogOwner = s_requestIdToSender[requestId];
+        //assign this NFT a token
+        //want to know how many counts we have actually minted
+        uint256 new_tokenCounter = s_tokenCounter;
+        s_tokenCounter = +1;
+        //this line of code has minted the nft
+        _safeMint(dogOwner, new_tokenCounter);
     }
 }
